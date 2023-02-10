cce0ed1b21d33f7bb272ee5a29bf960e581ea636
blckvia <a.vyatkinn@gmail.com>
2023-02-10 16:40:23 +0500

    done

--- src/app.ts
+++ src/app.ts
@@ -120,7 +120,7 @@ interface User {
   }
 
   // Async logic
-  async function getAllTodos() {
+  async function getAllTodos(): Promise<Todo[]> {
     try {
       const response = await fetch(
         'https://jsonplaceholder.typicode.com/todos?_limit=15'
@@ -132,10 +132,11 @@ interface User {
       if (error instanceof Error) {
         alertError(error);
       }
+      return [];
     }
   }
 
-  async function getAllUsers() {
+  async function getAllUsers(): Promise<User[]> {
     try {
       const response = await fetch(
         'https://jsonplaceholder.typicode.com/users?_limit=5'
@@ -147,6 +148,7 @@ interface User {
       if (error instanceof Error) {
         alertError(error);
       }
+      return [];
     }
   }
 
